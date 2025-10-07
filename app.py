eed20f130168226cfcaff03e8a2c1f3bf026e336
KhalidM23 <khalidmoham2324@gmail.com>
2025-10-07 08:42:47 -0700

    add database models and gitignore

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 
-from flask import Flask
+from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vacations.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
 @app.route('/')
 def home():
-    return "Welcome to the Vacation Sharing Platform!"
+    return render_template('home.html')
 
+@app.route('/vacation')
+def vacations():
+    return render_template('vacations.html')
+
+with app.app_context():
+    db.create_all()
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Vacation(db.Model):
+
+    __tablename__ = "vacations"
+    id = db.Column(db.Integer,primary_key =True)
+    destination = db.Column(db.String(100))
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    total_cost = db.Column(db.Float)
+    tips = db.Column(db.Text)
+    user_id = db.Column(db.Integer)
+    # Your job: Define the columns here
+    # Use: db.Column() with types like db.Integer, db.String(), db.Text, db.Date
+    # Example: id = db.Column(db.Integer, primary_key=True)
+
+class Itinerary(db.Model):
+    __tablename__ = "itineraries"
+    id = db.Column(db.Integer, primary_key = True)
+    activity = db.Column(db.String(100))
+    date_of_activity = db.Column(db.Date)
+    cost_of_activity = db.Column(db.Float)
+    vacation_id = db.Column(db.Integer)
+
